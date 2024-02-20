44855fa79f7262b7ddf3ed68d1c075388a1c919e
Colby <colby.braden@gmail.com>
2024-02-20 08:50:48 -0600

    updated dotfiles

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,4 @@
 alias v="vim"
-alias sway="seatd-launch dbus-run-session sway"
+alias sway="ck-launch-session seatd-launch dbus-run-session sway"
 alias reboot="sudo reboot"
 alias shutdown="sudo poweroff"
